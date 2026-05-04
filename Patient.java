edaeb93c1d5b459ebce6b82e3aebcd3521a5806b
Oscar Roncero <oscarroncer@gmail.com>
2026-05-04 19:14:07 +0200

    empezando funcion getFileListPaths

---
+++ Patient.java
@@ -0,0 +1,88 @@
+import java.time.*;
+
+public class Patient {
+    
+    // atributos de la clase
+    public String No;
+    public String ID;
+    public String FirstName;
+    public String MiddleName;
+    public String LastName;
+    public String Sex;
+    public int Age;
+    public LocalDate DOB;
+
+    //-------------------------------------------- GETTERS Y SETTERS -----------------------------------------------
+
+    // No
+    public String getNo() {
+        return this.No;
+    }
+
+    public void setNo(String no) {
+        this.No = no;
+    }
+
+    // ID
+    public String getID() {
+        return this.ID;
+    }
+
+    public void setID(String id) {
+        this.ID = id;
+    }
+
+    // FirstName
+    public String getFirstName() {
+        return this.FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
+    }
+
+    // MiddleName
+    public String getMiddleName() {
+        return this.MiddleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.MiddleName = middleName;
+    }
+
+    // LastName
+    public String getLastName() {
+        return this.LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    // Sex
+    public String getSex() {
+        return this.Sex;
+    }
+
+    public void setSex(String sex) {
+        this.Sex = sex;
+    }
+
+    // Age
+    public int getAge() {
+        return this.Age;
+    }
+
+    public void setAge(int age) {
+        this.Age = age;
+    }
+
+    // DOB
+    public LocalDate getDOB() {
+        return this.DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+}
---
+++ program.java
@@ -0,0 +1,173 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+
+public class program {
+
+    // en este metodo voy a hacer el flujo del programa, externalizando en funciones el parse, y la comprobacion de si 
+    // el dir esta vacio o no
+    public static void main(String[] args){
+        
+        Path dir = Paths.get("XML");
+        Path[] listadoFilePaths = getFileListPaths(); // iterarems por los paths de este array parseando todos los archivos PREGUNTAR SI SE QUIERE ASI
+
+        // nos lanzamos a ver si podemos ejecutar el codigo con un try catch
+        try {
+
+            // antes de ejecutar veremos si en el path hay algo, si no, no entramos en el programa, no vale la pena si no hay nada
+            if (pathVacio(dir)){    // si recibo true entro y ejecutare el parser, SINTAXIS: lo mismo que *== true
+
+                System.err.println("ha entrado donde empezaremos el parser");
+                //ejecutamos funcion del parser
+                XMLParser(dir);
+
+            }
+
+        } catch (Exception e) {
+
+            System.err.println("Error de E/S comprobando la carpeta: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+
+        }
+
+    }
+
+    // hacemos un proceso para saber si el path tiene algo
+    private static boolean pathVacio(Path dir) throws IOException {
+
+        // compruebo si hay algo en el path y devuelvo true
+        if (Files.exists(dir) && Files.isDirectory(dir)) {
+            
+            DirectoryStream<Path> stream = Files.newDirectoryStream(dir);
+            System.err.println("directorio existe");
+
+            if (stream.iterator().hasNext()){
+                
+                stream.close();
+                return true;
+                }
+            else{
+            stream.close();
+            return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    // esto tiene que iterar sobre todos los files de el directorio dir en variable global y ir sumando el path en objeto path al array
+    private static Path[] getFileListPaths(){
+
+        ArrayList<Path> filePaths = new ArrayList<Path>(); //inicializo array donde guardare todos los 
+
+        return filePaths.toArray(new Path[0]);
+
+
+
+    }
+
+    //va a haber que ponerle que se ejecute automaticamente mientras en el archivo haya cosas
+    private static void XMLParser(Path dir){
+
+        //hay que instanciar los lectores de eventos
+        try {
+
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(dir.toFile()));
+
+        //creamos los objetos para darles valor en memoria segun vayamos parseando
+        Common common = new Common();
+        //Patient patient = new Patient(); 
+
+        //LOGICA 
+
+        // aqui guardaremos lo que estamos trabajando, dandole el valor de START_ELEMENT a esta variable
+        // para que cuando llegue el CHARACTER sepamos a que atributo (porque tendra el valor de START_ELEMENT) 
+        // le daremos el valor de CHARACTER
+
+        String currentElement = "";
+            
+            while (reader.hasNext()) {
+
+                // se maneja el tipo de evento con numeros en esta libreria, guardamos ese numero para saber que tipo de evento es
+                int evento = reader.next(); 
+
+                // ESTE SWITCH VA A INDETIFICAR EL TIPO DE EVENTO Y ACTUARA SEGUN 
+                switch (evento) {
+                    case XMLStreamConstants.START_ELEMENT:
+                        currentElement =   reader.getLocalName(); //en este caso guardo el START_ELEMENT en current element, 
+                        break;
+                    
+                    case XMLStreamConstants.CHARACTERS:
+                        String texto = reader.getText().trim(); //estas funcionas consiguen el texto de CHARACTERS limpio para guardarlo
+                        
+                        if (!texto.isEmpty()){
+
+                            // hay que hacer que segun el campo que sea se guarde en el setter adecuado
+                            // problema si hubiera campos repetidos, pero creo que no hay en XML
+                            switch(currentElement){
+
+                                //los casos posibles de common
+                                case "Company": common.setCompany(texto);
+                                break;
+
+                                case "ModelName": common.setModelName(texto);
+                                break;
+
+                                case "MachineNo": common.setMachineNo(texto);
+                                break;
+
+                                case "ROMVersion": common.setROMVersion(texto);
+                                break;
+
+                                case "Version": common.setVersion(texto);
+                                break;
+
+                                case "Date": common.setDate(texto);
+                                break;
+                                
+                                case "Time": common.setTime(texto);
+                                break;
+
+
+                            }
+                        }
+                        break;
+
+                    case XMLStreamConstants.END_ELEMENT:
+                        currentElement = "";        //ponemos la variable currentElement a "" para volver a ejecutar el bucle en
+                                                    //con el siguiente
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+
+        }catch (Exception e){
+            System.err.println("error en linea 72: " + e);
+        }
+        finally{
+            
+        }
+
+
+
+    }
+    
+}
